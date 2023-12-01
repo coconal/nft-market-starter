b90ba9c0c43262aa9975c78ca9334af7f4c31758
coconal <m15196712167@163.com>
2023-12-01 21:32:54 +0800

    update

--- contracts/erc20-usdt.sol
+++ contracts/erc20-usdt.sol
@@ -2,3 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor()
+        ERC20("SToken", "USDT")
+    {
+        _mint(msg.sender, 1 * 10 ** 8 * 10 ** 18);
+    }
+
+}
